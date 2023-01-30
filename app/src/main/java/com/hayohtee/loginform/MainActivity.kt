055352c200cbebe8ec951ce9dbe8116a61946f5a
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2023-01-30 20:49:49 +0100

    Add hardcoded registered users value to MainActivity.kt

--- app/src/main/java/com/hayohtee/loginform/MainActivity.kt
+++ app/src/main/java/com/hayohtee/loginform/MainActivity.kt
@@ -2,10 +2,23 @@ package com.hayohtee.loginform
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+    companion object {
+        // map username with password for registered users
+        val registeredUsers = mapOf(
+            "john_doe" to "john123",
+            "marie.james" to "marie36",
+            "mad_dog" to "paul21",
+            "hayohtee" to "olamilekan47"
+        )
+    }
+
 }
