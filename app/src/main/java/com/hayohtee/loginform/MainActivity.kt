a603d277e2c454f9633e7d1666944bde67e8d8f2
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2023-01-30 22:27:18 +0100

    Update MainActivity.kt

--- app/src/main/java/com/hayohtee/loginform/MainActivity.kt
+++ app/src/main/java/com/hayohtee/loginform/MainActivity.kt
@@ -5,21 +5,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
-import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
-
-const val FORM_ACTIVITY_ID = 10
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val resultLauncher = registerForActivityResult(StartActivityForResult()) { result ->
-            if (result.resultCode == Activity.RESULT_OK) {
-                val username = result.data?.getStringExtra(MESSAGE_KEY)
-                findViewById<TextView>(R.id.welcome).text = username
+        val resultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val username = result.data?.getStringExtra(MESSAGE_KEY)
+                    findViewById<TextView>(R.id.welcome).text = username
+                }
             }
-        }
 
         val intent = Intent(this, FormActivity::class.java)
         resultLauncher.launch(intent)
